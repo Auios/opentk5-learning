bff4a3e3914e57131511653fbf2959d80070e69b
Connor O'Connor <LoneAuios@gmail.com>
2026-03-30 15:09:13 -0400

    textures

--- Program.cs
+++ Program.cs
@@ -15,9 +15,27 @@ public static class Program {
     Shader shader = new Shader("vert.glsl", "frag.glsl");
 
     GL.ClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+    GL.Enable(EnableCap.DepthTest);
 
-    Vector3[] verticies = [(-0.5f, -0.5f, 0.0f), (0.5f, -0.5f, 0.0f), (-0.5f, 0.5f, 0.0f),
-    (0.5f, -0.5f, 0.0f), (0.5f, 0.5f, 0.0f), (-0.5f, 0.5f, 0.0f)];
+    Vector3[] verticies = [
+      (-0.5f, -0.5f, -0.5f), (0.5f, -0.5f, -0.5f),(0.5f,  0.5f, -0.5f),
+      (0.5f,  0.5f, -0.5f), (-0.5f,  0.5f, -0.5f),(-0.5f, -0.5f, -0.5f),
+
+      (-0.5f, -0.5f,  0.5f), (0.5f, -0.5f,  0.5f),(0.5f,  0.5f,  0.5f),
+      (0.5f,  0.5f,  0.5f), (-0.5f,  0.5f,  0.5f),(-0.5f, -0.5f,  0.5f),
+
+      (-0.5f,  0.5f,  0.5f), (-0.5f,  0.5f, -0.5f),(-0.5f, -0.5f, -0.5f),
+      (-0.5f, -0.5f, -0.5f), (-0.5f, -0.5f,  0.5f),(-0.5f,  0.5f,  0.5f),
+
+      (0.5f,  0.5f,  0.5f), (0.5f,  0.5f, -0.5f),(0.5f, -0.5f, -0.5f),
+      (0.5f, -0.5f, -0.5f), (0.5f, -0.5f,  0.5f),(0.5f,  0.5f,  0.5f),
+
+      (-0.5f, -0.5f, -0.5f), (0.5f, -0.5f, -0.5f),(0.5f, -0.5f,  0.5f),
+      (0.5f, -0.5f,  0.5f), (-0.5f, -0.5f,  0.5f),(-0.5f, -0.5f, -0.5f),
+
+      (-0.5f,  0.5f, -0.5f), (0.5f,  0.5f, -0.5f),(0.5f,  0.5f,  0.5f),
+      (0.5f,  0.5f,  0.5f), (-0.5f,  0.5f,  0.5f),(-0.5f,  0.5f, -0.5f),
+    ];
 
     // GL.Enable(EnableCap.CullFace);
     // verticies = verticies.Reverse().ToArray();
@@ -32,14 +50,33 @@ public static class Program {
     uint position = (uint)GL.GetAttribLocation(shader.id, "position");
     GL.EnableVertexAttribArray(position);
     GL.VertexAttribPointer(position, 3, VertexAttribPointerType.Float, false, sizeof(float) * 3, 0);
+    int uTransformation = GL.GetUniformLocation(shader.id, "transform");
+    int uProjection = GL.GetUniformLocation(shader.id, "projection");
+
+    // Matrix4 projection = Matrix4.CreateOrthographic(2.66f, 2f, 0.01f, 100f);
+    Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(
+      MathHelper.DegreesToRadians(90f), 1.33f, 0.1f, 100f);
+
+    float angle = 0f;
 
     while (true) {
       Toolkit.Window.ProcessEvents(false);
       if (Toolkit.Window.IsWindowDestroyed(Window.handle)) break;
 
-      GL.Clear(ClearBufferMask.ColorBufferBit);
-      GL.DrawArrays(PrimitiveType.Triangles, 0, verticies.Length);
-      Window.SwapBuffers();
+      // Draw
+      {
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        Matrix4 transform = Matrix4.CreateRotationX(-angle)
+          * Matrix4.CreateRotationY(angle / 2f)
+          * Matrix4.CreateTranslation(0f, 0f, -2f)
+          * Matrix4.CreateScale(1.5f, 1.5f, 2f);
+        GL.UniformMatrix4f(uProjection, 1, true, in projection);
+        GL.UniformMatrix4f(uTransformation, 1, true, ref transform);
+        angle += 0.01f;
+        angle = MathHelper.ClampRadians(angle);
+        GL.DrawArrays(PrimitiveType.Triangles, 0, verticies.Length);
+        Window.SwapBuffers();
+      }
 
       Thread.Sleep(1);
     }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,7 +1,11 @@
 #version 330
 
 in vec3 position;
+out vec3 color;
+uniform mat4 transform;
+uniform mat4 projection;
 
 void main() {
-  gl_Position = vec4(position, 1.0);
+  color = position + vec3(0.5);
+  gl_Position = vec4(position, 1.0) * transform * projection;
 }
